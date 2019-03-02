45bc29649fc5caaa72bf634b7749306b3ce0293b
Buddhika85 <buddhika2006system@gmail.com>
2019-03-03 01:11:22 +0530

    2 initial components setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieItemComponent,
+    RecepieDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
